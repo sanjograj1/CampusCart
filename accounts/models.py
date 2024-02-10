1615f6b8a8907fabe43b4619191b642f1341963e
karanmahajan <mahaja81@uwindsor.ca>
2024-02-10 10:17:59 -0500

    adds the user profile pic functionality

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    profile_image = models.ImageField(upload_to='images/', null=True, blank=True, default='images/default.png')
+
+    def __str__(self):
+        return self.username
--- campuscart/settings.py
+++ campuscart/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'accounts'
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -118,10 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -135,3 +142,5 @@ EMAIL_PORT = 587
 EMAIL_FROM = "campuscartdjango@gmail.com"
 EMAIL_HOST_USER = "campuscartdjango@gmail.com"
 EMAIL_HOST_PASSWORD = "hqkkmzvaxdfpdwau"
+
+PASSWORD_RESET_TIMEOUT = 14800
