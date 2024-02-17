6ab6fbfa12e6f3a6ba007f40d710a9f8f341cca6
karanmahajan <karanmahajan321@gmail.com>
2024-02-17 17:31:03 -0500

    updated user profile image html

--- products/forms.py
+++ products/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from .models import Product
+from crispy_forms.layout import Layout, Submit, Field
+from crispy_forms.helper import FormHelper
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ["title", "description", "price", "image"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Field('title', css_class='form-control'),
+            Field('description', css_class='form-control'),
+            Field('price', css_class='form-control'),
+            Field('image', css_class='form-control'),
+        )     
