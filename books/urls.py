8df804d8d5fd1716abfd622bac4fcbd026a21461
Niharika Khurana <khurananiharika96@gmail.com>
2024-02-19 15:46:55 -0500

    updated model and urls

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+ 
 app_name = "books"
 urlpatterns = [
     path('', views.bookhome, name='home'),
     path('upload', views.upload_book, name='upload-book'),
+    path('book-detail/<int:bookid>', views.book_detail, name='book-detail'),
 ]
