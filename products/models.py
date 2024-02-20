55d77fb04630c9ddb3e9cb407f78e4ad57e42c49
sanjograj1 <sanjograj2@gmail.com>
2024-02-20 01:16:51 -0500

    Added model and updated url

--- products/models.py
+++ products/models.py
@@ -3,15 +3,30 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
+CATEGORY_LIST = [
+    ("Electronics", "Electronics"),
+    ("Clothing", "Clothing"),
+    ("Shoes", "Shoes"),
+    ("Books", "Books"),
+    ("Furniture", "Furniture"),
+    ("Others", "Others"),
+]
+
 
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField()
+    category = models.CharField(max_length=255, choices=CATEGORY_LIST)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to="product_images/")
+    interested_users = models.ManyToManyField(User, related_name="interested_products")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    # check product is interested by user or not
+    def is_interested(self, user):
+        return self.interested_users.filter(id=user.id).exists()
 
     def __str__(self):
         return self.title
