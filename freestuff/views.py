2a1dcf47c86a9a136be6740843cf7516c5697e69
Janvi Chhabra <chhabr12@uwindsor.ca>
2024-02-20 21:34:50 -0500

    updated the view for free stuff home page

--- freestuff/views.py
+++ freestuff/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import FreeStuffItem
 
-# Create your views here.
+def index(request):
+    items = FreeStuffItem.objects.all()
+    return render(request, 'freestuff/home.html', {
+        'items': items,
+        'title':'Free Stuff'
+    })
