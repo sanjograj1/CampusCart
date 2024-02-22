005974a3d848dddc6bfd4fa337cb02479f4581e6
Niharika Khurana <khurananiharika96@gmail.com>
2024-02-21 23:25:58 -0500

    added more fields to book model

--- books/models.py
+++ books/models.py
@@ -3,13 +3,34 @@ from django.contrib.auth.models import User
  
 # Create your models here.
 class Book(models.Model):
+    BOOKS_CATEGORY = [
+        ('Fiction', 'Fiction'),
+        ('Non-Fiction', 'Non-Fiction'),
+        ('Academic', 'Academic'),
+        ('Children', 'Children'),
+        ('Art & Photography', 'Art & Photography'),
+        ('Cookbooks', 'Cookbooks'),
+        ('Travel', 'Travel'),
+        ('Health & Wellness', 'Health & Wellness'),
+        ('Religion & Spirituality', 'Religion & Spirituality'),
+        ('Hobbies & Crafts', 'Hobbies & Crafts'),
+        ('Sports & Recreation', 'Sports & Recreation'),
+        ('Science Fiction & Fantasy', 'Science Fiction & Fantasy'),
+        ('Horror', 'Horror'),
+        ('Poetry', 'Poetry'),
+        ('Drama', 'Drama'),
+        ('Others', 'Others'),
+    ]
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     seller = models.ForeignKey(User,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    book_cover = models.ImageField(upload_to="upload_book/",default='upload_book/book-cover.png')
+    pages = models.PositiveIntegerField()
+    language = models.CharField(max_length=50,default="English")
+    category = models.CharField(max_length=50, choices=BOOKS_CATEGORY,default="Others",blank=True)
+    book_cover = models.ImageField(upload_to="upload_book/",default='upload_book/book-cover.png',blank=True)
  
  
     def __str__(self):
