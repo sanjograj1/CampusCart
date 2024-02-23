38df301c225d51d1e34eb8ae4fbb653d4a51fde4
karanmahajan <karanmahajan321@gmail.com>
2024-02-22 23:35:30 -0500

    added url and removed context from settings

--- campuscart/settings.py
+++ campuscart/settings.py
@@ -58,7 +58,6 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                'accounts.context.profile_image',
             ],
         },
     },
