337e565535b3dba4f9829b832f5e53215cc5a17a
noobcoder996 <purpleemperor96@gmail.com>
2024-02-27 00:13:33 -0500

    Added views and URLs

--- lostfound/views.py
+++ lostfound/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+
+@login_required
+def index(request):
+    return render(request, 'lostfound/home.html', {
+        'title': 'Lost & Found'
+    })
