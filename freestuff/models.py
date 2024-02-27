e8ad8d3cd67eb93e4e73e13e641e6446a430e101
Janvi Chhabra <chhabr12@uwindsor.ca>
2024-02-26 21:49:56 -0500

    added new fields in free stuff

--- freestuff/models.py
+++ freestuff/models.py
@@ -4,12 +4,26 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class FreeStuffItem(models.Model):
+    CATEGORY_LIST = [
+    ("Electronics", "Electronics"),
+    ("Clothing", "Clothing"),
+    ("Shoes", "Shoes"),
+    ("Furniture", "Furniture"),
+    ("Book", "Book"),
+    ("Others", "Others"),
+    ]
+    CONDITION_LIST = [
+        ("New","New"),
+        ("Used","Used"),
+    ]
     title = models.CharField(max_length=255)
     description = models.TextField()
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     item_image = models.ImageField(upload_to='freestuff/', default='freestuff/default.png')
     created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=255, choices=CATEGORY_LIST, blank=True, null=True,default="Others")
+    condition = models.CharField(max_length=255, choices=CONDITION_LIST, default="New")
 
     def __str__(self):
         return self.title
