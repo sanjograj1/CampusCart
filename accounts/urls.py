5d271ede629c6c24b5a0d47b475b46f50b88f289
Kajal Singh <kajalsingh@Kajals-MacBook-Air.local>
2024-03-04 00:16:20 -0500

    added contact us urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("logout/", views.user_logout, name="logout"),
     path("profile/", views.profile, name="profile"),
+    path("profile/listing/", views.user_listing, name="user-listing"),
+    path("profile/rating/<username>", views.user_rating, name="user-rating"),
+    path('notifications/', views.notifications_view, name='notifications'),
     path('contactus/', views.contactus, name='contactus'),
-
 ]
