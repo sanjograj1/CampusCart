f1b0fab5054873c3b5ac224ecc530f3730c80b15
Kajal Singh <kajalsingh@Kajals-MacBook-Air.local>
2024-03-04 00:14:30 -0500

    created db table for contact us

--- accounts/models.py
+++ accounts/models.py
@@ -7,11 +7,19 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
-    profile_image = models.ImageField(upload_to="profile_images/")
+    profile_image = models.ImageField(upload_to="profile_images/",default='profile_images/default.png')
 
     def __str__(self):
         return f"Profile of {self.user.username}"
 
+class UserComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    commented_by = models.ForeignKey(User,on_delete=models.CASCADE,related_name="commented_by")
+    comment = models.TextField()
+    commented_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Rating for {self.user.username}'
 
 class Contact(models.Model):
 
