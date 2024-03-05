0f74107354c1af38e941455cfa743e7a19f3f77a
Haard-Mehta <mehta1e@uwindsor.ca>
2024-03-04 20:28:04 -0500

    added upload event url and updated view

--- events/urls.py
+++ events/urls.py
@@ -5,5 +5,5 @@ app_name = "events"
 urlpatterns = [
     path('', views.eventshome, name='event-home'),
     path('detail/', views.eventdetail, name='event-detail'),
-
+    path('upload-event/', views.uploadevent, name='upload-event'),
 ]
