dac7d6e6673cf5dea5394be8b85676582a7b156a
Kajal Singh <kajalsingh@Kajals-MacBook-Air.local>
2024-03-07 15:01:29 -0500

    model for contact us

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile,UserComment
+from .models import Profile,UserComment, Contact
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(UserComment)
+admin.site.register(Contact)
