e7fbef9c9274c52ce849aa958bd740f8d926faae
Janvi Chhabra <chhabr12@uwindsor.ca>
2024-03-10 13:35:19 -0400

    added url and view for edit free item

--- freestuff/urls.py
+++ freestuff/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('upload', views.upload_item, name='upload-item'),
     path('item-detail/<int:itemid>', views.item_detail, name='item-detail'),
+    path('edit/<int:itemid>', views.edit_item, name='edit-item'),
 ]
