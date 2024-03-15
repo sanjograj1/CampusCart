eb0c7288eee4dcc0cade8b9b3a205849c69a9948
Janvi Chhabra <chhabr12@uwindsor.ca>
2024-03-15 13:16:02 -0400

    added edit property and view property.html

--- rentals/views.py
+++ rentals/views.py
@@ -29,4 +29,32 @@ def upload_property(request):
         form = property_form()
     return render(request, 'rental/upload_property.html',{'title':'upload New Property','form':form})
 
+@login_required
+def edit_property(request,rentid):
+    current_property=get_object_or_404(Rental,pk=rentid)
+    if current_property.seller != request.user:
+        messages.success(request, "You don't have the access to this property", extra_tags='danger')
+    if request.method=='POST':
+        if 'action' in request.POST:
+            form=property_form(request.POST,request.FILES,instance=current_property)
+            if form.is_valid():
+                prop=form.save(commit=False)
+                prop.save()
+                return redirect('accounts:user-listing')
+        else:
+            current_property.delete()
+            messages.success(request, "This property / rental has been deleted.", extra_tags='danger')
+            return redirect('accounts:user-listing')
+    else:
+        form=property_form(instance=current_property)
+    return render(request,'rental/edit-property.html',{'form':form,'title':f'Edit {current_property.property_name}'})
+
         
+@login_required
+def property_detail(request, rentid):
+    current_rental = get_object_or_404(Rental, pk=rentid)
+    rental_list=Rental.objects.all()
+    # get same city rentals
+    same_city_rentals = Rental.objects.exclude(pk=current_rental.id).filter(city=current_rental.city)
+ 
+    return render(request, 'rental/property-detail.html',{'title':f'{current_rental.property_name} details','same_city_rentals':same_city_rentals, 'rental' : current_rental})
