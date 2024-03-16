762cdd7009d86df5a1fba1a1dc054acd238c3b01
noobcoder996 <purpleemperor96@gmail.com>
2024-03-16 16:50:16 -0400

    models.py for lostfound app

--- lostfound/models.py
+++ lostfound/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class LostandfoundItem(models.Model):
+    
+    category_choice = [
+        ('LOST', 'Lost'),
+        ('FOUND', 'Found')
+    ]
+
+    category = models.CharField(max_length=5, choices=category_choice)
+    title = models.CharField(max_length=100)
+    product_description = models.TextField()
+    post_date = models.DateTimeField(auto_now_add = True)
+    image = models.ImageField(upload_to='lost_and_found_images/')
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
