2f429b186807d7ce72eee1bdda183d85cb29abef
Kajal Singh <kajalsingh@Kajals-MacBook-Air.local>
2024-03-20 01:46:01 -0400

    added urls and updated login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import (
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
+
 
 app_name = "accounts"
 urlpatterns = [
@@ -15,6 +22,35 @@ urlpatterns = [
         views.toggle_sold_status,
         name="toggle-sold-status",
     ),
+     path(
+        "password-reset/",
+        PasswordResetView.as_view(
+            template_name="accounts/password_reset.html",
+        ),
+        name="password-reset",
+    ),
+    path(
+        "password-reset/done/",
+        PasswordResetDoneView.as_view(
+            template_name="accounts/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="accounts/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        PasswordResetCompleteView.as_view(
+            template_name="accounts/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+
     path("profile/rating/<username>", views.user_rating, name="user-rating"),
     path('notifications/', views.notifications_view, name='notifications'),
     path('contactus/', views.contactus, name='contactus'),
