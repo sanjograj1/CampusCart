1add17abd3f855d1f928d0e7cb363ac9e76a005e
Janvi Chhabra <chhabr12@uwindsor.ca>
2024-03-20 23:17:36 -0400

    added html and css for filters and added modelAdmin

--- freestuff/admin.py
+++ freestuff/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from .models import FreeStuffItem
-
+ 
+ 
+class FreeStuffItemAdmin(ModelAdmin):
+    list_display = ['title', 'condition', 'seller','is_sold']
+ 
+ 
 # Register your models here.
-admin.site.register(FreeStuffItem)
+admin.site.register(FreeStuffItem, FreeStuffItemAdmin)
