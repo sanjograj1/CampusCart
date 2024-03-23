e7ad3db8ea67367ad3b3cee3b8e14e29d5c80c91
karanmahajan <karanmahajan321@gmail.com>
2024-03-22 22:31:00 -0400

    fixed lost and found

--- lostfound/models.py
+++ lostfound/models.py
@@ -4,19 +4,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class LostandfoundItem(models.Model):
-    
     category_choice = [
         ('LOST', 'Lost'),
         ('FOUND', 'Found')
     ]
-
     category = models.CharField(max_length=5, choices=category_choice, blank=False)
     title = models.CharField(max_length=100, blank =False)
     product_description = models.TextField(blank =False)
     post_date = models.DateTimeField(auto_now_add = True)
     image = models.ImageField(upload_to='lost_and_found_images/', blank =False)
     location = models.CharField(max_length=100, blank =False)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
 
 
     def __str__(self):
