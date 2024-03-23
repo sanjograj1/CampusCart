b1ea16e0cf855a23ac11821aea6b63e6aa7f4a92
kajal30011996 <kajal.singh30011996@gmail.com>
2024-03-23 00:56:39 -0400

    New Pass-Key for email verification

--- campuscart/settings.py
+++ campuscart/settings.py
@@ -133,7 +133,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_FROM = "campuscartdjango@gmail.com"
 EMAIL_HOST_USER = "campuscartdjango@gmail.com"
-EMAIL_HOST_PASSWORD = "hqkkmzvaxdfpdwau"
+EMAIL_HOST_PASSWORD = "lxvcnaewmweyzpot"
 
 PASSWORD_RESET_TIMEOUT = 14800
 
